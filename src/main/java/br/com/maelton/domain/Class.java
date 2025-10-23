f04c08280e61e74d8b88133654c8350128acd06b
Maelton Lima <maelton.main@gmail.com>
2025-10-23 10:31:37 -0300

    feat(domain): add domain classes

---
+++ src/main/java/br/com/maelton/domain/Class.java
@@ -0,0 +1,11 @@
+package br.com.maelton.domain;
+
+public class Class {
+    private int id;
+    private int size;
+    
+    public Class(int id, int size) {
+        this.id = id;
+        this.size = size;
+    }   
+}
---
+++ src/main/java/br/com/maelton/domain/Classroom.java
@@ -0,0 +1,11 @@
+package br.com.maelton.domain;
+
+public class Classroom {
+    private int id;
+    private int capacity;
+    
+    public Classroom(int id, int capacity) {
+        this.id = id;
+        this.capacity = capacity;
+    }
+}
